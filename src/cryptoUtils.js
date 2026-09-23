b27f5fc29910f73cef4eff98b44226241da51d84
Thameem Ashraf <ayman@Aymans-Mac.local>
2026-09-23 10:16:11 +0530

    fix: replace removed crypto APIs

--- src/cryptoUtils.js
+++ src/cryptoUtils.js
@@ -4,18 +4,47 @@ function hashValue(text) {
   return crypto.createHash('sha256').update(text).digest('hex');
 }
 
+function deriveKey(key) {
+  return crypto.createHash('sha256').update(key).digest();
+}
+
 function encryptValue(text, key) {
-  const cipher = crypto.createCipher('aes-256-cbc', key);
-  let encrypted = cipher.update(text, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return encrypted;
+  const encryptionKey = deriveKey(key);
+  const iv = crypto.randomBytes(16);
+
+  const cipher = crypto.createCipheriv(
+    'aes-256-cbc',
+    encryptionKey,
+    iv
+  );
+
+  const encrypted = Buffer.concat([
+    cipher.update(text, 'utf8'),
+    cipher.final()
+  ]);
+
+  return Buffer.concat([iv, encrypted]).toString('hex');
 }
 
 function decryptValue(encrypted, key) {
-  const decipher = crypto.createDecipher('aes-256-cbc', key);
-  let decrypted = decipher.update(encrypted, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted;
+  const encryptionKey = deriveKey(key);
+  const data = Buffer.from(encrypted, 'hex');
+
+  const iv = data.subarray(0, 16);
+  const ciphertext = data.subarray(16);
+
+  const decipher = crypto.createDecipheriv(
+    'aes-256-cbc',
+    encryptionKey,
+    iv
+  );
+
+  const decrypted = Buffer.concat([
+    decipher.update(ciphertext),
+    decipher.final()
+  ]);
+
+  return decrypted.toString('utf8');
 }
 
 module.exports = { hashValue, encryptValue, decryptValue };
