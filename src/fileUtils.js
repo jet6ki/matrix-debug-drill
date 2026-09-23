134154375336fa638c659a0948724a6511ea9330
Thameem Ashraf <ayman@Aymans-Mac.local>
2026-09-23 10:18:56 +0530

    fix: make file utilities cross-platform

--- src/fileUtils.js
+++ src/fileUtils.js
@@ -1,16 +1,17 @@
 const fs = require('fs');
+const path = require('path');
 
 function readConfig(configName) {
-  const configPath = __dirname + '/configs/' + configName + '.json';
+  const configPath = path.join(__dirname, 'configs', `${configName}.json`);
   return JSON.parse(fs.readFileSync(configPath, 'utf8'));
 }
 
 function getOutputPath(filename) {
-  return __dirname + '/output/' + filename;
+  return path.join(__dirname, 'output', filename);
 }
 
 function readTextFile(filepath) {
-  return fs.readFileSync(filepath, 'utf8');
+  return fs.readFileSync(filepath, 'utf8').replace(/\r\n/g, '\n');
 }
 
 module.exports = { readConfig, getOutputPath, readTextFile };
